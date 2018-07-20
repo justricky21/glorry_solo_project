3f565f7ef77f6edcf7150ec1429b6796cd563155
Ricardo Ruiz <rfpimpao@gmail.com>
2018-07-20 16:11:21 +0100

    adventurer class tested for propertirs

--- models/adventurer.rb
+++ models/adventurer.rb
@@ -1,7 +1,15 @@
 class Adventurer
-
-  def initialize(options)
-
+  attr_accessor :name, :title, :class, :level, :status, :adventure, :background
+  attr_reader :id
+  def initialize(option)
+    @id = option['id'].to_i
+    @name = option['name']
+    @title = option['title']
+    @class = option['class']
+    @level = option['level'].to_i
+    @status = option['status']
+    @adventure = option['adventure']
+    @background = option['background']
   end
 
 end
--- models/specs/adventurer_spec.rb
+++ models/specs/adventurer_spec.rb
@@ -2,19 +2,26 @@ require('minitest/autorun')
 require('minitest/rg')
 require_relative('../adventurer')
 
-def TestAdventurer < MiniTest::Test
+class TestAdventurer < MiniTest::Test
 
   def setup
-    aragorn = Adventurer.new('name' => 'Aragorn',
-    'title' => 'Longshanks'
-    'class' => 'Ranger'
-    'level' => '12'
-    'status' => 'Adventuring'
-    'adventure' => 'The Fellowship of the Ring'
+    @aragorn = Adventurer.new('name' => 'Aragorn',
+    'title' => 'Longshanks',
+    'class' => 'Ranger',
+    'level' => '12',
+    'status' => 'Adventuring',
+    'adventure' => 'The Fellowship of the Ring',
     'background' => 'Speaks Elvish fluently. Ingenious, very skilled with both
     sword and bow. Incredibly knowledgeable of herbs and remedies. Works very
     well in a team.')
   end
 
-  
+  def test_get_name
+    assert_equal('Aragorn', @aragorn.name)
+  end
+
+  def test_get_level
+    assert_equal(12, @aragorn.level)
+  end
+
 end
