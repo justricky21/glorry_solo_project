79437a5506f8cafa718f3035d1fdb773ab2cc5ad
Ricardo Ruiz <rfpimpao@gmail.com>
2018-07-23 11:12:47 +0100

    incremented the seed file. started the driver controller

---
+++ controllers/deliveries_controller.rb
@@ -0,0 +1,6 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative( '../models/driver.rb' )
+require_relative( '../models/customer.rb' )
+require_relative( '../models/delovery.rb' )
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,15 @@ Customer.delete_all()
 
 driver1 = Driver.new('name' => 'Ben Thomas',
 'lorry' => 'Fiat Ducato',
-'lorry_plate' => 'BD51SMR',
-'current_delivery' => 'Some balls of cotton')
+'lorry_plate' => 'BD51SMR')
+
+driver2 = Driver.new('name' => 'Joseph Serengetti',
+'lorry' => 'Iveco Daily',
+'lorry_plate' => 'IT15PGT')
+
+driver3 = Driver.new('name' => 'Takashi Ideki',
+'lorry' => 'Isuzu Trucks Forward',
+'lorry_plate' => 'JP45SHI')
 
 customer1 = Customer.new('company_name' => 'Tesco Ltd.',
 'contact_name' => 'Laura Tate, Mrs',
@@ -18,11 +25,29 @@ customer1 = Customer.new('company_name' => 'Tesco Ltd.',
 'email' => 'laura.tate@tesco.co.uk'
 )
 
+customer2 = Customer.new('company_name' => 'Sainsburys Ltd.',
+'contact_name' => 'Mary Bennett, Ms',
+'phone_number' => '101231212',
+'address' => 'Elstree Distribution Centre, Elstree Way, Borehamwood, Hertfordshire WD6 1SN',
+'email' => 'mary.bennet@sainsburys.co.uk'
+)
+
+customer3 = Customer.new('company_name' => 'ASDA Inc.',
+'contact_name' => 'John Smith, Mr',
+'address' => 'Asda Distribution Centre, Lymedale Business Park, Dalewood Road, Lymedale Business Park, Staffordshire, ST5 9QH',
+'phone_number' => '101235039',
+'email' => 'john_smith9@asda.co.uk'
+)
+
 delivery1 = Delivery.new('customer_id' => 1,
   'driver_id' => 1,
   'contents' => 'Some cotton balls'
 )
 
 driver1.save
+driver2.save
+driver3.save
 customer1.save
+customer2.save
+customer3.save
 delivery1.save
